ae74dfa2944eb7ea1b19462a12471946bf2d6622
Greatman Ugorji <coderyker@gmail.com>
2023-08-21 16:43:01 +0100

    added active state for ratings

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ btn.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
+
+// active state
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');        
+    });
+});
